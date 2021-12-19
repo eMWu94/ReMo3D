960ac2b67214245971e219159b3312842dba7a30
eMWu94 <81933368+eMWu94@users.noreply.github.com>
2021-12-19 16:04:06 +0100

    Add files via upload
    Upload of the setup file and src directory

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='remo3d',
+    version='1.0.0',
+    author='Michał Wilkosz',
+    author_email='michal.m.wilkosz@gmail.com',
+    description='ReMo3D is a Python package that allows to generate synthetic normal and lateral logs for complex 2D and 3D models',
+    py_modules=['remo3d'],
+    package_dir={'': 'src'},
+    install_requires= ['numpy', 'matplotlib', 'mpi4py'],
+    license='LGPL-2.1 License'
+)
