b46805206d49ae30c73609c5f643269923ba5cab
eMWu94 <81933368+eMWu94@users.noreply.github.com>
2022-05-08 12:11:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='remo3d',
     version='1.0.0',
-    author='Michał Wilkosz',
-    author_email='michal.m.wilkosz@gmail.com',
+    author='',
+    author_email='',
     description='ReMo3D is a Python package that allows to generate synthetic normal and lateral logs for complex 2D and 3D models',
     py_modules=['remo3d', 'worker_2D', 'worker_3D'],
     package_dir={'': 'src'},
-    install_requires= ['numpy', 'matplotlib', 'mpi4py'],
+    install_requires= ['numpy', 'scipy', 'matplotlib', 'mpi4py'],
     license='LGPL-2.1 License'
 )
